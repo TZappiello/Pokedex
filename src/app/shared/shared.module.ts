2b236560e38f5f4f3d161d0afeed3a48cd70451d
Tzappiello <tzappiello@gmail.com>
2022-08-26 13:55:02 -0300

    Criação dos components de pesquisa e de listar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 
 @NgModule({
   declarations: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
