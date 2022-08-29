fd10e0cd9713bb0556f575d12dd4ea0cda79ec74
Tzappiello <tzappiello@gmail.com>
2022-08-29 16:49:35 -0300

    Criação do método que lista apenas o detalhe do pokemon selecionado

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.pokemons
   }
 
+  get pokemons() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res)
+      }
+    );
+  }
 }
