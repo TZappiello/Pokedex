f36e79069eb040aa0d8571ac46ebe33f7c0e0dc3
Tzappiello <tzappiello@gmail.com>
2022-08-29 14:22:01 -0300

    Populando as informações e carregando na tela

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,13 +8,18 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  getAllPokemons: any;
+
   constructor(
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
     this.pokeApiService.pokeApiList.subscribe(
-      res => console.log(res)
+      res => {
+        this.getAllPokemons = res.results;
+        // console.log(this.getAllPokemons)
+      }
     )
   };
 
