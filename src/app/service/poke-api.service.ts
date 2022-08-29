f9b8283738d479aa518f781ac0c6f600c59ccd0d
Tzappiello <tzappiello@gmail.com>
2022-08-29 20:43:40 -0300

    Diminuir a quantidade de cards

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -7,7 +7,7 @@ import { map, Observable, tap } from 'rxjs';
 })
 export class PokeApiService {
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50';
 
   constructor(
     private http: HttpClient
